3d88dd3d36ede836e5a17e107fa2dbcf9b0baed0
MacBobby Chibuzor <macbobbychibuzor@MacBobbys-MacBook-Pro.local>
2022-12-19 22:57:07 +0100

    almost done, because ileri is pushing me 😜

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"github.com/jaegertracing/jaeger-client-go"
+	"github.com/opentracing/opentracing-go"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jaegertracing/jaeger-client-go/config"
 )
 
 func main() {
+	// Create Jaeger tracer
+	cfg := config.Configuration{
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+	tracer, _, err := cfg.New("kubernetes-jaeger-go-service", config.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(err)
+	}
+	opentracing.SetGlobalTracer(tracer)
+
 	router := mux.NewRouter()
-	router.HandleFunc("/", func(writer http.ResponseWriter, reader *http.Request) {
-		writer.Write([]byte("This application demos a web service deployed on Kubernetes."))
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Start a new span
+		span := opentracing.StartSpan("/")
+		defer span.Finish()
+
+		w.Write([]byte("This application demos a web service deployed on Kubernetes."))
 	})
 	http.ListenAndServe(":8080", router)
 }
